54c3e38c74279ddedebf76148ce3fd6c5b571664
Vamsibudumuru <vamsibudumuru2003@gmail.com>
2026-03-23 13:11:50 +0530

    STATEMENTS

---
+++ day4.py
@@ -0,0 +1,98 @@
+'''
+###if statement --> (THIS IF STATEMENT IS USED TO CHECK ANY CONDITION ,IF THE CONDITION BECOMES TRUE THEN IT WILL ENTER
+IN THE SIDE)'''
+
+
+'''
+age = int(input("enter your age:"))
+if age>= 18:
+    print("Your age is 18 or above")
+    '''
+
+
+'''
+student_att = int(input("pls enter your sem attendance:"))
+if student_att >= 75:
+    print("you can directly sit in the sem exam")
+'''
+
+
+'''
+student_fee = int(input("please enter the addmission_fee:"))
+if student_fee >= 5000:
+    print("you are not eligible for exam")
+    '''
+'''
+###if-else statement---> THIS ALSO CALLED AS FALL BACK STATEMENTS WHICH ONLY EXECUTE
+WHEN THE (IF STATEMENT) BECOMES FALSE.'''
+
+
+'''
+total_am = int(input("enter the total shopping money:"))
+if total_am >= 1498:
+    print("no delivery cost")
+else:
+    print(f"add{149 - total_am} to your cart")       '''
+
+    
+'''
+### if-elif-else statement(if+else)--- in the elif part, i can '''
+
+'''
+student_marks = int(input("enter yor marks :"))
+if student_marks >= 90:
+    print("your got A+ grade")
+elif student_marks >= 75 and student_marks <90:
+    print("your got A grade")
+elif student_marks >= 60 and student_marks <75:
+    print("your got B+ grade")
+else:
+    print("your fail")
+
+    '''
+
+
+'''
+num_1 = int(input("enter 1st number:"))
+num_2 = int(input("enter 2st number:"))
+user_choice =(input("Enter your choice \n1.Add \n2.Sub \n3.Multiply \n4.divide"))
+if user_choice =="+":
+   print(num_1 + num_2)
+elif user_choice == "-":
+    print(num_1 - num_2)
+
+'''
+
+
+'''
+user_choice = int(input("please enter any number:"))
+if user_choice % 2 == 0:
+    print(f"{user_choice} is a even number")
+else:
+    print(f"{user_choice} is a odd number")
+    
+    '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
