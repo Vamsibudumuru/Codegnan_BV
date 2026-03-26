9b44bdab78e6b1682600ed698cfb327bf0acd2b0
Vamsibudumuru <vamsibudumuru2003@gmail.com>
2026-03-26 12:52:09 +0530

    TUPLES

---
+++ day 5.py
@@ -0,0 +1,148 @@
+'''
+print(9+8)
+print("python" + "language")
+print([1+3] + [2+4])
+'''
+
+'''
+##CONATENATION
+#--------------
+##THIS IS NOTHING BUT ,A (+) BEHAVI....
+#CASE-1
+#-------
+#INTEGER---- THIS WILL ACT AS ADDDITION FOR THE INT
+
+##CASE2
+#-------
+#NOTE:- YOU CAN ONLY CONCATENATE STR (NOT "LIST") TO STR
+##FOR OTHER DATATYPES (WE HAVE TO USE SAME TYPES)THIS (+)ACT AS CONCATENATON
+
+
+
+### TUPLE()----->
+#----------------
+I## IS A COLLECTION OF DIFFERENT DATATYPES AND THIS IS REPRESENTED BY(),
+# , SEPERATED BY (,)'''
+
+'''THING = (1,"TEJA,[12,4],(6,7))'''
+
+'''
+thing = (12,89,"python",(21,"vamsi",67,"python is a programming language"'''
+
+
+
+#SWAPPING QUESTION
+'''
+Num = 9
+Num_2 = 90
+print(f"before swapping Num ={Num} and Num_2 ={Num_2}")
+Num,Num_2 = Num_2,Num
+print(f"After swapping Num ={Num} and Num_2 ={Num}")
+'''
+
+#LEAP YEAR
+'''
+leap_year = int(input("enter year:"))
+if (leap_year % 400 == 0 and leap_year % 100 != 0)or leap_year % 400 ==0:
+    
+    print(f"YES, {leap_year} is a leap year")
+
+else:
+    print(f"NO,{leap_year} is not leap year")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
